44f2fc59f880c07267a7031785d729c70be9bf8e
ashiq73 <ashiq.buet73@gmail.com>
2024-09-29 23:19:08 +0600

    post design done

--- src/components/cards/PostCard.tsx
+++ src/components/cards/PostCard.tsx
@@ -1,28 +1,80 @@
-import React from 'react';
-import {Avatar} from "@nextui-org/react";
-import { Ellipsis, Globe,  } from 'lucide-react';
-import PostActionDropDown from './cardsComp/PostActionDropDown';
+import React from "react";
+import { Avatar, Divider, Image } from "@nextui-org/react";
+import { Globe, ThumbsDown, ThumbsUp } from "lucide-react";
 
+import PostActionDropDown from "./cardsComp/PostActionDropDown";
 
 const PostCard = () => {
-    return (
-        <div className='p-4 bg-sky-900/25 dark:bg-slate-800/45 rounded-md'>
-            <div className='px-5'>
-            <div className="flex gap-3 items-center">
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-   <div className='flex justify-between  w-full'>
-   <p className='flex flex-col'> {"Ashik Mahmud"}
-   <small className='text-[10px] flex items-center gap-1 text-slate-300/75'> <Globe size={11}/> published: {"12:20pm"}</small>
-   </p>
-   
- <PostActionDropDown/>
-   </div>
-      
-    </div>
+  return (
+    <div className="p-4 bg-sky-900/25 dark:bg-slate-800/45 rounded-md">
+      <div className="px-5">
+        {/* post top  */}
+        <div className="flex gap-3 items-center">
+          <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+          <div className="flex justify-between  w-full">
+            <p className="flex  font-medium flex-col">
+              {" "}
+              {"Ashik Mahmud"}
+              <small className="text-[10px] flex text-black items-center gap-1 dark:text-slate-300/75">
+                {" "}
+                <Globe size={11} /> published: {"12:20pm"}
+              </small>
+            </p>
 
-            </div>
+            <PostActionDropDown />
+          </div>
         </div>
-    );
+
+ {/* post main body  */}
+ <section className="space-y-2 py-2">
+     {/* if text post available show  */}
+     {/* todo condition  */}
+     <p className="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto at, reprehenderit totam rem sed dolorem eveniet minima magnam voluptate incidunt recusandae distinctio illum, unde consectetur delectus blanditiis consequuntur nisi.
+     </p>
+     {/* if image here show  */}
+     <div>
+        <Image
+       className="object-cover h-full w-full"
+        src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+     </div>
+     <div className="flex justify-between items-center py-2">
+        <p className="flex items-center gap-4">
+            <span className="flex gap-1"><ThumbsUp/>
+            {"10"}</span>
+           <span className="flex gap-1">
+           <ThumbsDown/>
+           {4}
+           </span>
+        </p>
+
+        <p className="flex items-center">
+            {22} comments
+        </p>
+     </div>
+     {/* comment collection  */}
+     <Divider/>
+     <div className="py-2 flex gap-1.5">
+<Avatar size="sm" src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
+<div className="bg-slate-400/45 w-fit px-2 rounded-xl">
+<p className="text-[11px]">{"Ashiqeee"}</p>
+<p className="text-[12px]">আপেল ফোন নাকি ভাইয়া.....😮😮😮😮, আজকাল চোখে ম্যালা কম দেহি..</p>
+</div>
+     </div>
+     <div className="py-2 flex gap-1.5">
+<Avatar size="sm" src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
+<div className="bg-slate-400/45 w-fit px-2 rounded-xl">
+<p className="text-[11px]">{"Ashiqeee"}</p>
+<p className="text-[12px]">আপেল ফোন নাকি ভাইয়া.....😮😮😮😮, আজকাল চোখে ম্যালা কম দেহি..</p>
+</div>
+     </div>
+
+
+ </section>
+
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
--- src/components/cards/cardsComp/PostActionDropDown.tsx
+++ src/components/cards/cardsComp/PostActionDropDown.tsx
@@ -1,30 +1,53 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import {
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownTrigger,
-  } from "@nextui-org/dropdown";
-import { Ellipsis } from 'lucide-react';
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/dropdown";
+import { Delete, Edit, Ellipsis } from 'lucide-react';
 
 const PostActionDropDown = () => {
-    return (
-        <Dropdown>
-        <DropdownTrigger>
-          <button className='rounded-full hover:bg-slate-800/45 px-2 '><Ellipsis className='text-blue-800' /></button>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Static Actions">
-          <DropdownItem >
-            edit
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dropdown
+      isOpen={isOpen}
+      onOpenChange={(open) => setIsOpen(open)} // Handle opening/closing from other triggers as well
+    >
+      <DropdownTrigger
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+      >
+        <button className="rounded-full hover:bg-slate-800/45 px-2">
+          <Ellipsis className="text-blue-800" />
+        </button>
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label="Post Actions"
+        onMouseEnter={() => setIsOpen(true)}  // Keep the dropdown open while hovering over it
+        onMouseLeave={() => setIsOpen(false)} // Close the dropdown when the mouse leaves
+      >
+         <DropdownItem
+            key="edit"
+            description="Allows you to edit the file"
+             startContent={<Edit  />}
+          >
+            Edit file
           </DropdownItem>
-          <DropdownItem >
-            delete
-          </DropdownItem>
-        
-    
-        </DropdownMenu>
-      </Dropdown>
-    );
+        <DropdownItem
+          key="delete"
+          className="text-danger"
+          color="danger"
+          description="Permanently delete the file"
+          startContent={<Delete className={"text-danger"} />}
+        >
+            Delete post
+            </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
 };
 
 export default PostActionDropDown;

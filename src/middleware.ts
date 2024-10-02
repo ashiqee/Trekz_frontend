a02e0ea96b842ab75be1020b90efe83867f65589
ashiq73 <ashiq.buet73@gmail.com>
2024-10-03 01:02:31 +0600

    middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,65 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/AuthService";
+
+
+
+const authRoutes = ["/login",'/register'];
+
+type Role = keyof typeof roleBasedRoutes;
+
+const roleBasedRoutes={
+    ADMIN:[/^\/admin/,/profile/],
+    USER:[/^\/user/,/profile/],
+   
+}
+
+export async function middleware(request:NextRequest){
+    const {pathname}= request.nextUrl;
+
+    const user = await getCurrentUser();
+  
+
+
+    if(!user){
+        if(authRoutes.includes(pathname)){
+            return NextResponse.next();
+        }else{
+            return NextResponse.redirect(
+                new URL(
+                    pathname ? `/login?redirect=${pathname}`:"/login",
+                    request.url
+                )
+            );
+        }
+    }
+    
+    // Role Base Authorization 
+
+    if(user?.role && roleBasedRoutes[user?.role as Role]){
+        const routes = roleBasedRoutes[user?.role as Role];
+
+        if(routes.some((route)=>pathname.match(route))){
+            return NextResponse.next();
+        }
+    }
+
+    return NextResponse.redirect(new URL('/',request.url));
+
+
+}
+
+
+
+
+
+export const config ={
+
+    matcher :[
+        '/login',
+        '/register',
+        "/profile",
+        '/admin/:page',
+        '/user/:page',
+        
+    ]
+}
